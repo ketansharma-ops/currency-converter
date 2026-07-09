04232b790cd769af13291d7f930c757d4b9c337c
Ketan Sharma <ketansharma.engineer@gmail.com>
2026-07-09 21:16:06 +0530

    first_time_upload

---
+++ script.js
@@ -0,0 +1,213 @@
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+let fromamt=document.querySelector("#from-amt");
+let toamt=document.querySelector("#to-amt");
+let options1=document.querySelector("#from_opt");
+let options2=document.querySelector("#to_opt");
+for(key in countryList){
+    let el1=document.createElement("option");
+    let el2=document.createElement("option");
+    el1.value=key;
+    el2.value=key;
+    el1.innerText=key;
+    el2.innerText=key;
+    options1.append(el1);
+    options2.append(el2);
+}
+options2.value="INR";
+options1.value="USD";
+const changeflag=(ele)=>{
+    let countryname=ele.value;
+    let countrycode=countryList[countryname];
+    let el=`https://flagsapi.com/${countrycode}/shiny/64.png`;
+    let Source=ele.parentElement.querySelector("img");
+    Source.src=el;
+};
+changeflag(options1);
+changeflag(options2);
+options1.addEventListener("change",(evl)=>{
+    changeflag(evl.target);
+});
+options2.addEventListener("change",(evl)=>{
+    changeflag(evl.target);
+});
+let exchange=document.querySelector(".icon-select");
+exchange.addEventListener("click",async (ele)=>{
+    ele.preventDefault();
+    let amt=Number(fromamt.value);
+    if (!amt) {
+        alert("Enter an amount");
+        return;
+    }
+    let from=options1.value;
+    let to=options2.value;
+    let rate= await getRate(from,to);
+    toamt.value=(amt*rate).toFixed(2);
+});
+async function getRate(from,to) {
+    from = from.toLowerCase();
+    to = to.toLowerCase();
+    const URL = `https://latest.currency-api.pages.dev/v1/currencies/${from}.json`;
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data[from][to];
+}
